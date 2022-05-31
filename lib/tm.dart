81bc5b0b1098390361752de3428618e0a529efca
Crypto Kira <git@wong.so>
2022-05-31 21:50:56 +0800

    Initialize example application

--- lib/tm.dart
+++ lib/tm.dart
@@ -1,7 +1,36 @@
 library tm;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class TimeMachine extends StatefulWidget {
+  const TimeMachine({
+    Key? key,
+    this.size = const Size(double.infinity, double.infinity),
+    this.backgroundColor = Colors.white,
+  }) : super(key: key);
+
+  final Size size;
+  final Color backgroundColor;
+
+  factory TimeMachine.observatory() {
+    return const TimeMachine();
+  }
+
+  @override
+  State<TimeMachine> createState() => _TimeMachineState();
+}
+
+class _TimeMachineState extends State<TimeMachine> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints.expand(
+        width: widget.size.width,
+        height: widget.size.height,
+      ),
+      decoration: BoxDecoration(
+        color: widget.backgroundColor,
+      ),
+    );
+  }
 }
